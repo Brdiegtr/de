84d541637918581bfd7b573617a75e1c9a6b5a63
Brdiegtr <arria487@gmail.com>
2025-05-29 12:03:12 -0600

    segundo avance

--- client/src/app/Servicios/socket.service.ts
+++ client/src/app/Servicios/socket.service.ts
@@ -1,5 +1,25 @@
-// src/app/Servicios/socket.service.ts
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 
-@Injectab
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io('http://localhost:5000');
+  }
+
+  listen(event: string, callback: (...args: any[]) => void): void {
+    this.socket.on(event, callback);
+  }
+
+  emit(event: string, data: any): void {
+    this.socket.emit(event, data);
+  }
+
+  removeListener(event: string, callback: (...args: any[]) => void): void {
+    this.socket.off(event, callback);
+  }
+}
